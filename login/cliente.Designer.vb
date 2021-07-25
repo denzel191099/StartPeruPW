b6d513c16f5c530d11d54f91bd67c0196defc23f
jegs08 <jegs08@EDUARDO-DESKTOP>
2021-07-24 22:07:28 -0500

    cambio

--- login/cliente.Designer.vb
+++ login/cliente.Designer.vb
@@ -40,20 +40,21 @@ Partial Class cliente
         Me.btnclientemodificar = New System.Windows.Forms.Button()
         Me.Label7 = New System.Windows.Forms.Label()
         Me.txtcodigocliente = New System.Windows.Forms.TextBox()
-        Me.Panel1 = New System.Windows.Forms.Panel()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.Label8 = New System.Windows.Forms.Label()
         CType(Me.dgcliente, System.ComponentModel.ISupportInitialize).BeginInit()
-        Me.Panel1.SuspendLayout()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(125, 280)
+        Me.Label4.BackColor = System.Drawing.Color.Transparent
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.White
+        Me.Label4.Location = New System.Drawing.Point(101, 276)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(86, 13)
+        Me.Label4.Size = New System.Drawing.Size(118, 18)
         Me.Label4.TabIndex = 16
         Me.Label4.Text = "Apellido Materno"
         '
@@ -68,9 +69,12 @@ Partial Class cliente
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(125, 230)
+        Me.Label3.BackColor = System.Drawing.Color.Transparent
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.White
+        Me.Label3.Location = New System.Drawing.Point(101, 226)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(84, 13)
+        Me.Label3.Size = New System.Drawing.Size(115, 18)
         Me.Label3.TabIndex = 14
         Me.Label3.Text = "Apellido Paterno"
         '
@@ -85,9 +89,12 @@ Partial Class cliente
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(125, 187)
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(101, 183)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(47, 13)
+        Me.Label2.Size = New System.Drawing.Size(66, 18)
         Me.Label2.TabIndex = 12
         Me.Label2.Text = "Nombre:"
         '
@@ -102,9 +109,12 @@ Partial Class cliente
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(125, 146)
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(101, 142)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(29, 13)
+        Me.Label1.Size = New System.Drawing.Size(37, 18)
         Me.Label1.TabIndex = 10
         Me.Label1.Text = "DNI:"
         '
@@ -118,6 +128,7 @@ Partial Class cliente
         '
         'dgcliente
         '
+        Me.dgcliente.BackgroundColor = System.Drawing.Color.CadetBlue
         Me.dgcliente.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
         Me.dgcliente.Location = New System.Drawing.Point(390, 75)
         Me.dgcliente.Name = "dgcliente"
@@ -135,18 +146,24 @@ Partial Class cliente
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(128, 329)
+        Me.Label5.BackColor = System.Drawing.Color.Transparent
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.White
+        Me.Label5.Location = New System.Drawing.Point(104, 325)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(42, 13)
+        Me.Label5.Size = New System.Drawing.Size(58, 18)
         Me.Label5.TabIndex = 19
         Me.Label5.Text = "Celular:"
         '
         'Label6
         '
         Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(128, 378)
+        Me.Label6.BackColor = System.Drawing.Color.Transparent
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.ForeColor = System.Drawing.Color.White
+        Me.Label6.Location = New System.Drawing.Point(104, 374)
         Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(41, 13)
+        Me.Label6.Size = New System.Drawing.Size(59, 18)
         Me.Label6.TabIndex = 21
         Me.Label6.Text = "Correo:"
         '
@@ -159,28 +176,43 @@ Partial Class cliente
         '
         'btninsertarcliente
         '
-        Me.btninsertarcliente.Location = New System.Drawing.Point(170, 427)
+        Me.btninsertarcliente.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.btninsertarcliente.FlatAppearance.BorderSize = 0
+        Me.btninsertarcliente.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btninsertarcliente.Image = CType(resources.GetObject("btninsertarcliente.Image"), System.Drawing.Image)
+        Me.btninsertarcliente.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btninsertarcliente.Location = New System.Drawing.Point(845, 504)
         Me.btninsertarcliente.Name = "btninsertarcliente"
-        Me.btninsertarcliente.Size = New System.Drawing.Size(75, 23)
+        Me.btninsertarcliente.Size = New System.Drawing.Size(97, 40)
         Me.btninsertarcliente.TabIndex = 22
-        Me.btninsertarcliente.Text = "Insertar"
-        Me.btninsertarcliente.UseVisualStyleBackColor = True
+        Me.btninsertarcliente.Text = "Insertar  "
+        Me.btninsertarcliente.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.btninsertarcliente.UseVisualStyleBackColor = False
         '
         'btnclientemodificar
         '
-        Me.btnclientemodificar.Location = New System.Drawing.Point(285, 427)
+        Me.btnclientemodificar.BackColor = System.Drawing.Color.PaleTurquoise
+        Me.btnclientemodificar.FlatAppearance.BorderSize = 0
+        Me.btnclientemodificar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnclientemodificar.Image = CType(resources.GetObject("btnclientemodificar.Image"), System.Drawing.Image)
+        Me.btnclientemodificar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btnclientemodificar.Location = New System.Drawing.Point(948, 504)
         Me.btnclientemodificar.Name = "btnclientemodificar"
-        Me.btnclientemodificar.Size = New System.Drawing.Size(75, 23)
+        Me.btnclientemodificar.Size = New System.Drawing.Size(96, 40)
         Me.btnclientemodificar.TabIndex = 23
-        Me.btnclientemodificar.Text = "Modificar"
-        Me.btnclientemodificar.UseVisualStyleBackColor = True
+        Me.btnclientemodificar.Text = "Modificar  "
+        Me.btnclientemodificar.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.btnclientemodificar.UseVisualStyleBackColor = False
         '
         'Label7
         '
         Me.Label7.AutoSize = True
-        Me.Label7.Location = New System.Drawing.Point(125, 101)
+        Me.Label7.BackColor = System.Drawing.Color.Transparent
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.ForeColor = System.Drawing.Color.White
+        Me.Label7.Location = New System.Drawing.Point(101, 97)
         Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(43, 13)
+        Me.Label7.Size = New System.Drawing.Size(60, 18)
         Me.Label7.TabIndex = 25
         Me.Label7.Text = "Codigo:"
         '
@@ -192,21 +224,12 @@ Partial Class cliente
         Me.txtcodigocliente.Size = New System.Drawing.Size(100, 20)
         Me.txtcodigocliente.TabIndex = 24
         '
-        'Panel1
-        '
-        Me.Panel1.BackColor = System.Drawing.Color.DarkSeaGreen
-        Me.Panel1.Controls.Add(Me.Label8)
-        Me.Panel1.Controls.Add(Me.PictureBox1)
-        Me.Panel1.Location = New System.Drawing.Point(-3, -1)
-        Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(1275, 23)
-        Me.Panel1.TabIndex = 26
-        '
         'PictureBox1
         '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Transparent
         Me.PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), System.Drawing.Image)
         Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
-        Me.PictureBox1.Location = New System.Drawing.Point(7, 3)
+        Me.PictureBox1.Location = New System.Drawing.Point(6, 4)
         Me.PictureBox1.Name = "PictureBox1"
         Me.PictureBox1.Size = New System.Drawing.Size(20, 19)
         Me.PictureBox1.TabIndex = 0
@@ -215,7 +238,9 @@ Partial Class cliente
         'Label8
         '
         Me.Label8.AutoSize = True
-        Me.Label8.Location = New System.Drawing.Point(27, 6)
+        Me.Label8.BackColor = System.Drawing.Color.Transparent
+        Me.Label8.ForeColor = System.Drawing.Color.Cornsilk
+        Me.Label8.Location = New System.Drawing.Point(26, 7)
         Me.Label8.Name = "Label8"
         Me.Label8.Size = New System.Drawing.Size(39, 13)
         Me.Label8.TabIndex = 1
@@ -225,10 +250,12 @@ Partial Class cliente
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ControlLight
         Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
         Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
         Me.ClientSize = New System.Drawing.Size(1082, 632)
-        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.Label7)
         Me.Controls.Add(Me.txtcodigocliente)
         Me.Controls.Add(Me.btnclientemodificar)
@@ -251,32 +278,28 @@ Partial Class cliente
         Me.Name = "cliente"
         Me.Text = "cliente"
         CType(Me.dgcliente, System.ComponentModel.ISupportInitialize).EndInit()
-        Me.Panel1.ResumeLayout(False)
-        Me.Panel1.PerformLayout()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-
-    Friend WithEvents Label4 As Label
     Friend WithEvents txtapmcliente As TextBox
-    Friend WithEvents Label3 As Label
     Friend WithEvents txtappcliente As TextBox
-    Friend WithEvents Label2 As Label
     Friend WithEvents txtnomcliente As TextBox
-    Friend WithEvents Label1 As Label
     Friend WithEvents txtdnicliente As TextBox
     Friend WithEvents dgcliente As DataGridView
     Friend WithEvents txtcel As TextBox
-    Friend WithEvents Label5 As Label
-    Friend WithEvents Label6 As Label
     Friend WithEvents txtcorreocliente As TextBox
     Friend WithEvents btninsertarcliente As Button
     Friend WithEvents btnclientemodificar As Button
-    Friend WithEvents Label7 As Label
     Friend WithEvents txtcodigocliente As TextBox
-    Friend WithEvents Panel1 As Panel
     Friend WithEvents Label8 As Label
     Friend WithEvents PictureBox1 As PictureBox
+    Public WithEvents Label4 As Label
+    Public WithEvents Label3 As Label
+    Public WithEvents Label2 As Label
+    Public WithEvents Label1 As Label
+    Public WithEvents Label5 As Label
+    Public WithEvents Label6 As Label
+    Public WithEvents Label7 As Label
 End Class
